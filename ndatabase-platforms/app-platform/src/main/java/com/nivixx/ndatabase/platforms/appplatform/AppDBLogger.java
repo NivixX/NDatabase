0b88bba9a4f13b4b0992f9d9d1eee33276313fc9
Fabian <fabian.phan@gmail.com>
2023-02-06 19:42:57 +0900

    Support modern java version & bukkit 1.19+

--- ndatabase-platforms/app-platform/src/main/java/com/nivixx/ndatabase/platforms/appplatform/AppDBLogger.java
+++ ndatabase-platforms/app-platform/src/main/java/com/nivixx/ndatabase/platforms/appplatform/AppDBLogger.java
@@ -1,8 +1,6 @@
 package com.nivixx.ndatabase.platforms.appplatform;
 
 import com.nivixx.ndatabase.platforms.coreplatform.logging.DBLogger;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -14,25 +12,27 @@ public class AppDBLogger extends DBLogger {
         super(isDebugMode);
     }
 
-    private static final Logger LOGGER = LogManager.getLogger(AppDBLogger.class);
 
     @Override
     public Consumer<Supplier<String>> consumeDebugMessage() {
-        return (msg) -> LOGGER.debug(msg.get());
+        return (msg) -> System.out.println(msg.get());
     }
 
     @Override
     public Consumer<Supplier<String>> consumeInfoMessage() {
-        return (msg) -> LOGGER.info(msg.get());
+        return (msg) -> System.out.println(msg.get());
     }
 
     @Override
     public BiConsumer<Throwable, String> consumeErrorMessage() {
-        return (throwable, msg) -> LOGGER.error(msg, throwable);
+        return (throwable, msg) -> {
+            System.out.println(msg);
+            throwable.printStackTrace();
+        };
     }
 
     @Override
     public Consumer<Supplier<String>> consumeWarnMessage() {
-        return (msg) -> LOGGER.warn(msg.get());
+        return (msg) -> System.out.println(msg.get());
     }
 }
