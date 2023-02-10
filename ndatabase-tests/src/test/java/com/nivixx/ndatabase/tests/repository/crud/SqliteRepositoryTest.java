3826dfcbc5c2e79def887b4465dd8bb772b2aab7
Fabian <fabian.phan@gmail.com>
2023-02-10 22:04:29 +0900

    test ci fix

--- ndatabase-tests/src/test/java/com/nivixx/ndatabase/tests/repository/crud/SqliteRepositoryTest.java
+++ ndatabase-tests/src/test/java/com/nivixx/ndatabase/tests/repository/crud/SqliteRepositoryTest.java
@@ -5,7 +5,6 @@ import com.nivixx.ndatabase.api.exception.NDatabaseException;
 import com.nivixx.ndatabase.api.exception.NDatabaseLoadException;
 import com.nivixx.ndatabase.api.repository.Repository;
 import com.nivixx.ndatabase.core.config.DatabaseType;
-import com.nivixx.ndatabase.core.config.MysqlConfig;
 import com.nivixx.ndatabase.core.config.NDatabaseConfig;
 import com.nivixx.ndatabase.core.config.SqliteConfig;
 import com.nivixx.ndatabase.platforms.appplatform.AppNDatabaseConfig;
@@ -19,6 +18,9 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -28,10 +30,12 @@ public class SqliteRepositoryTest extends AbstractRepositoryTest {
     }
 
     @Before
-    public void initApp() throws NDatabaseLoadException, SQLException, IOException {
+    public void initApp() throws NDatabaseLoadException, SQLException, IOException, URISyntaxException {
         String jdbcURL = "jdbc:h2:mem:test;MODE=MYSQL";
         ExecutorService mainThread = Executors.newFixedThreadPool(1);
 
+        URL url = getClass().getClassLoader().getResource("testsqlite.sqlite");
+        File file = Paths.get(url.toURI()).toFile();
         appPlatformLoader = new AppPlatformLoader() {
             @Override
             public SyncExecutor supplySyncExecutor() {
@@ -42,15 +46,19 @@ public class SqliteRepositoryTest extends AbstractRepositoryTest {
             public NDatabaseConfig supplyNDatabaseConfig() {
                 AppNDatabaseConfig bukkitNDatabaseConfig = new AppNDatabaseConfig();
                 SqliteConfig sqliteConfig = new SqliteConfig();
-                sqliteConfig.setFileFullPath("src/test/resources/testsqlite.sqlite");
+                sqliteConfig.setFileFullPath(url.getPath());
                 bukkitNDatabaseConfig.setDatabaseType(DatabaseType.SQLITE);
                 bukkitNDatabaseConfig.setSqliteConfig(sqliteConfig);
                 return bukkitNDatabaseConfig;
             }
         };
-        File dbFile = new File("src/test/resources/testsqlite.sqlite");
+        /*
+        File dbFile = new File(resource.getPath());
+        Files.createDirectories(Paths.get(resource.getPath()));
         dbFile.createNewFile();
         dbFile.deleteOnExit();
+
+         */
         appPlatformLoader.load();
         repository = NDatabase.api().getOrCreateRepository(PlayerEntity.class);
     }
