3271c1e2063a39edd0e48c1ed765e2aebafe7e00
Fabian <fabian.phan@gmail.com>
2023-12-12 13:26:06 +0100

    Better connection pool handling for Sqlite

--- ndatabase-core/src/main/java/com/nivixx/ndatabase/core/dao/sqlite/SqliteConnectionPool.java
+++ ndatabase-core/src/main/java/com/nivixx/ndatabase/core/dao/sqlite/SqliteConnectionPool.java
@@ -1,5 +1,6 @@
 package com.nivixx.ndatabase.core.dao.sqlite;
 
+import com.nivixx.ndatabase.api.exception.NDatabaseException;
 import com.nivixx.ndatabase.core.config.SqliteConfig;
 import com.nivixx.ndatabase.core.dao.DatabaseConnection;
 import com.nivixx.ndatabase.core.dao.jdbc.JdbcConnectionPool;
@@ -33,26 +34,30 @@ public class SqliteConnectionPool implements JdbcConnectionPool {
         if(connection != null && !connection.isClosed()){
             return connection;
         }
-        try {
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            dbLogger.logError(e, "Failed to find sqlite driver, verify the sqlite driver is present in your java execution environment");
-            return null;
-        }
+
+        requireSqliteDriver();
+
         File dataFolder = new File(sqliteConfig.getFileFullPath());
         if (!dataFolder.exists()) {
             try {
                 dataFolder.createNewFile();
             } catch (IOException e) {
                 String msg = String.format("Failed to read/create sqlite file '%s'", sqliteConfig.getFileFullPath());
-                dbLogger.logError(e, msg);
+                throw new NDatabaseException(msg, e);
             }
         }
 
-        if(connection != null && !connection.isClosed()){
-            return connection;
-        }
         connection = DriverManager.getConnection("jdbc:sqlite:" + dataFolder);
         return connection;
     }
+
+    private void requireSqliteDriver() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            String msg = "Failed to find sqlite driver, verify the sqlite driver is present in your java execution environment";
+            throw new NDatabaseException(msg, e);
+        }
+    }
+
 }
